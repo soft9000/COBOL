37242f57b6c6f1f64ee4d4808307b85560c45ffa
Randall Nagy <randall.nagy@gmail.com>
2023-12-29 08:02:23 -0500

    Banner CALLable.

---
+++ TheCommons/BANNER/BANNER_EXAMPLE2.cob
@@ -0,0 +1,11 @@
+*> Mission: Demonstrate a call to another procedure (PROC_).
+*> GnuCOBOL: cobc -x -j -free BANNER_EXAMPLE2.cob PROC_BANNER2.cob
+*> Source: https://github.com/soft9000/COBOL/tree/master/TheCommons/BANNER
+IDENTIFICATION DIVISION.
+PROGRAM-ID. BANNER-EXAMPLE2.
+PROCEDURE DIVISION.
+DISPLAY ' '.
+CALL 'PROC_BANNER' USING '65.42.21'.
+DISPLAY ' '.
+DISPLAY "EXAMPLE2 ENDS".
+GOBACK.
